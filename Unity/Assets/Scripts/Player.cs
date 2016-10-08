ec16a95b3e4f6cf177bde78e4f6216b3090f975f
Jean-Louis Boudrand <jeanlouis.boudrand@hotmail.fr>
2016-10-08 22:21:15 +0200

    Player movement

--- Unity/Assets/Scripts/Player.cs
+++ Unity/Assets/Scripts/Player.cs
@@ -2,29 +2,65 @@
 using System.Collections;
 
 public class Player : MonoBehaviour {
-    public Rigidbody2D rb;
-    public float velocity;
+    public float _speed = 1f;
+	public float _maxAngle = 1f;
+
+	public float _offsetY = 0;
+	public float _radius = 1f;
+	public float _scaleX;
+
+	private float _angle = 0;
+	private Rigidbody2D _rb;
+	private float _movement = 0;
+
+
 	// Use this for initialization
 	void Start () {
-        velocity = 2;
-        rb = GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-
+	void Update ()
+	{
+		_angle += _movement;
+		newPos ();
 	}
+
+	void newPos()
+	{
+		_angle = Mathf.Clamp (_angle, -_maxAngle, _maxAngle);
+
+		float angle = _angle - (3.14f / 2f);
+
+
+		float x = Mathf.Cos (angle) * _radius * _scaleX;
+		float y = Mathf.Sin (angle) * _radius + _offsetY;
+		transform.position = new Vector3 (x, y, 0);
+	}
+
     public void GoLeft()
     {
-        rb.velocity = Vector2.left * velocity;
+		_movement -= _speed;
+
+
+        //rb.velocity = Vector2.left * velocity;
     }
     public void GoRight()
     {
-        rb.velocity = Vector2.right * velocity;
+		_movement += _speed;
+
+
+        //rb.velocity = Vector2.right * velocity;
     }
+
+	public void Stop()
+	{
+		_movement = 0;
+	}
+
     public void ResetVelocity()
     {
         Debug.Log("test");
-        rb.velocity = Vector2.zero;
+        _rb.velocity = Vector2.zero;
     }
 }
