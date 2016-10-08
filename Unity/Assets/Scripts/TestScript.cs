3db5bb1a8257ef71c34437bc8371a9c50f0b867a
Jean-Louis Boudrand <jeanlouis.boudrand@hotmail.fr>
2016-10-08 12:10:06 +0200

    Scene test

---
+++ Unity/Assets/Scripts/TestScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestScript : MonoBehaviour
+{
+
+	public float speed = 1f;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody2D> ().velocity = Vector2.up;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		gameObject.transform.Rotate(new Vector3(0, 0, Time.deltaTime * speed));
+		Debug.Log ("Pos = " + transform.position.x);
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		
+		Debug.Log ("COLLISION avec " + coll.collider.gameObject.name);
+	}
+
+	public void onClick()
+	{
+		Debug.Log ("Je clic");
+	}
+}
---
+++ Unity/Assets/Spawner.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject spawn;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 50; i++)
+			GameObject.Instantiate (spawn);
+	}
+
+}
