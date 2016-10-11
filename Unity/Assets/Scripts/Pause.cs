54b1de69b83e39308fcd3844681bda1706052e97
Paul FIGIEL <paulfigiel@laposte.net>
2016-10-11 14:41:50 +0200

    pause button

---
+++ Unity/Assets/Scripts/Pause.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+    private bool paused = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void stopTime()
+    {
+        if (paused)
+        {
+            Time.timeScale = 1;
+            paused = false;
+        }
+        else
+        {
+            Time.timeScale = 0;
+            paused = true;
+        }
+    }
+}
